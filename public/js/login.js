3b69850a3a2efcef5c662a2e912d957afb140c4e
Sam Rapowitz <samrapow@gmail.com>
2022-02-12 13:58:49 -0500

    Updated login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,1 +1,15 @@
-function grabLoginInfo
+const loginEl = document.querySelector('#login-button');
+
+
+
+
+function grabLoginInfo() {
+    console.log('clicked');
+    let usernameEl = document.querySelector('#typeUsernameX-2').value
+    let passwordEl = document.querySelector('#typePasswordX-2').value
+    console.log(usernameEl);
+    console.log(passwordEl);
+}
+
+
+loginEl.addEventListener('click', grabLoginInfo);
