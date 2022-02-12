6b4f1ec9f20b2b676d7b7739860c6f6133056c4f
JCWashington <jasmine.washington412@gmail.com>
2022-02-11 22:56:42 -0500

    updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,16 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.JAWS_DB ? process.env.JAWS_DB : process.env.MYSQL_URL
+const sequelize = process.env.JAWS_DB 
   //[scheme://][user[:[password]]@]host[:port][/schema]
-);
+  ? new Sequelize (process.env.JAWS_DB)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    dialectOptions: {
+      decimalNumbers: true,
+    },
+    logging: false,
+  });
 
 module.exports = sequelize;
