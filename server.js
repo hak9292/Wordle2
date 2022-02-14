e28eb296e7396dfde61d74c8ca9c0bb453a48cf6
tripledawg <iamlesliecrouch>
2022-02-14 17:45:39 -0500

    added seqeulizeStore to server.js

--- server.js
+++ server.js
@@ -2,20 +2,34 @@ const express = require('express');
 const controllers = require('./controllers');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+const session = require('express-session');
 // import sequelize connection
 const sequelize = require('./config/connection');
-const { original } = require('parseurl');
+const sequelizeStore = require('connect-session-sequelize')(session.Store); //also needed to store sessions???
+// const { original } = require('parseurl');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//handlebars
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+//middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+//router
 app.use(controllers);
+
+// Set up sessions
+const sess = {
+  secret: 'Super secret secret',
+  resave: false,
+  saveUninitialized: true,
+};
+app.use(session(sess));
 
 // sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false }).then(() => {
