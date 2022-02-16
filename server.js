57d371345bd1f4391c41166c0d2bf67fcac4ea8a
JCWashington <jasmine.washington412@gmail.com>
2022-02-15 20:03:56 -0500

    3rd times the charm

--- server.js
+++ server.js
@@ -1,34 +1,15 @@
 const path = require('path');
 const express = require('express');
-<<<<<<< HEAD
 const session = require('express-session');
 const app = express();
 
 const sequelize = require('./config/connection');
 const sequelizeStore = require('connect-session-sequelize')(session.Store);
-=======
-// express session
-const session = require('express-session');
-// import sequelize connection
-
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
-
->>>>>>> 051769653b7f690bff94c5ceb2cb8de0e5f44a1d
 
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 
-<<<<<<< HEAD
 const controllers = require('./controllers');
-=======
-const sess = {
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUnitialized: true
-};
-app.use(session(sess));
->>>>>>> 051769653b7f690bff94c5ceb2cb8de0e5f44a1d
 
 
 const sess = {
@@ -43,12 +24,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-<<<<<<< HEAD
 //router
 app.use(controllers);
-=======
-app.use(routes);
->>>>>>> 051769653b7f690bff94c5ceb2cb8de0e5f44a1d
 
 // Express setup
 const sess = {
