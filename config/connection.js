be92a2ce124c17bdb152c3d544f1ca2cb35ea361
tripledawg <iamlesliecrouch>
2022-02-15 23:44:37 -0500

    modified connection.js for heroku connection, erased force:false from line 36 of server.js

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,33 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = process.env.JAWS_DB 
-  ? new Sequelize (process.env.JAWS_DB)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
-    dialect: 'mysql',
-    dialectOptions: {
-      decimalNumbers: true,
-    },
-    logging: false,
-  });
+// const sequelize = process.env.JAWS_DB 
+//   ? new Sequelize (process.env.JAWS_DB)
+//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {  //not sure if this is still needed 
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     dialectOptions: {
+//       decimalNumbers: true,  //not sure if this is still needed 
+//     },
+//     logging: false,
+//   });
+
+//creating an instance of sequelize 
+let sequelize;
+  if (process.env.JAWSDB_URL) {
+    
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+  } else {
+    sequelize = new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PW,
+      {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306,
+      },
+    );
+  }
 
 module.exports = sequelize;
