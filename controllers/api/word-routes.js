9c31d9e4b30edc4c76e2e94850048e3862979423
hak9292 <ak112997@gmail.com>
2022-02-19 00:01:28 -0500

    el fin

--- controllers/api/word-routes.js
+++ controllers/api/word-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { multiply } = require('lodash');
 const sequelize = require('sequelize');
 const { Words } = require('../../models');
 
@@ -16,5 +17,18 @@ router.get('/getWord', async (req, res) => {
         res.status(500).json ({ message: 'Error retrieving word'});
     }
 });
-
+router.post('/checkWord', async (req, res) => {
+    try {
+        console.log(req.body.reqWord);
+        const checkWord = await Words.findOne({ where: { word: req.body.reqWord } });
+        var result = 0;
+        if (checkWord) {
+            result = 1;
+        }
+        res.status(200).json(result);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json ({ message: 'Error retrieving word'});
+    }
+});
 module.exports = router;
